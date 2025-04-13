774631e38599e596a5d03bdba162b42f5f24f152
Bahram <baxram1997007@gmail.com>
2025-04-13 10:46:05 +0400

    firsrt commit

---
+++ app/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { Quote } from "@/models/quote";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+	const [quotes, setQuotes] = useState<Quote[]>([]);
+
+	useEffect(() => {
+		const fetchQuotes = async () => {
+			const response = await fetch(
+				"http://localhost:3000/quotes/random?limit=10"
+			);
+
+			setQuotes(await response.json());
+		};
+		fetchQuotes();
+	}, []);
+
+	return (
+		<div className="p-4">
+			<h1 className="text-center text-3xl mb-6 dark:text-white">
+				Quotes
+			</h1>
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+				{quotes.map((quote) => (
+					<div
+						key={quote.id}
+						className="bg-white 
+						dark:bg-gray-800
+						p-4 shadow-md rounded-lg"
+					>
+						<p
+							className="mb-4 text-xl italic
+							text-gray-900
+						dark:text-gray-100
+						"
+						>
+							{quote.text}
+						</p>
+						<p
+							className="mb-10 text-right text-xl font-semibold
+						text-gray-700
+						dark:text-gray-300"
+						>
+							- {quote.author}
+						</p>
+						<div className="flex flex-wrap mt-2">
+							{quote.categories.map((category) => (
+								<span
+									key={category}
+									className="text-lg bg-gray-200
+									dark:bg-gray-700
+									text-gray-700 
+									dark:text-gray-300
+									px-2 py-1 rounded-full mr-2 mb-2"
+								>
+									{category}
+								</span>
+							))}
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
