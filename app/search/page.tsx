db1c3d70732345ae43c329e4bf57ffe7a5286d16
Бахрам Байрамзаде <baxram@MacBook-Air-Bahram.local>
2025-04-30 02:19:04 +0400

    added a validations for search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Button from "../components/Button";
 import Quotes from "../components/Quotes";
 
+const CATEGORY_NAME_REGEX = /^[a-z0-9\-]+$/;
+
 const createSearchQuery = (text: string, author: string, category: string) => {
 	const params = new URLSearchParams();
 
@@ -16,13 +18,19 @@ const createSearchQuery = (text: string, author: string, category: string) => {
 	return params.toString();
 };
 
+interface ValidationErrors {
+	text?: string;
+	author?: string;
+	category?: string;
+}
+
 const Search = () => {
 	const [text, setText] = useState<string>("");
 	const [author, setAuthor] = useState<string>("");
 	const [category, setCategory] = useState<string>("");
 	const [quotes, setQuotes] = useState<Quote[]>([]);
 	const [searchSubmitted, setSearchSubmitted] = useState<boolean>(false);
-	useState<boolean>(false);
+	const [errors, setErrors] = useState<ValidationErrors>({});
 
 	const handleSearch = async () => {
 		try {
@@ -38,36 +46,111 @@ const Search = () => {
 		}
 	};
 
+	const clearInputs = async () => {
+		setText("");
+		setAuthor("");
+		setCategory("");
+	};
+
+	const getValidationMessage = (setter: string, value: string) => {
+		if (setter === "text" && value && value.length < 2) {
+			return "Text must be at least 2 characters long.";
+		}
+		if (setter === "author" && value && value.length < 2) {
+			return "Author must be at least 2 characters long.";
+		}
+		if (
+			setter === "category" &&
+			value &&
+			!CATEGORY_NAME_REGEX.test(value)
+		) {
+			return "Category can only contain lowercase letters, numbers, and dashes";
+		}
+	};
+
+	const handleInputChange = (setter: string, value: string) => {
+		switch (setter) {
+			case "text":
+				setText(value);
+				break;
+			case "author":
+				setAuthor(value);
+				break;
+			case "category":
+				setCategory(value);
+				break;
+		}
+
+		const validationMessage = getValidationMessage(setter, value);
+
+		setErrors((prev) => {
+			const newErrors = { ...prev };
+			if (validationMessage) {
+				newErrors[setter as keyof ValidationErrors] = validationMessage;
+			}
+			if (!validationMessage) {
+				delete newErrors[setter as keyof ValidationErrors];
+			}
+			return newErrors;
+		});
+	};
+
 	return (
 		<div className="p-4">
 			<h1 className="text-3xl mb-6 text-center dark:text-white">
 				Search Quotes
 			</h1>
-			<div className="text-xl grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-				<input
-					type="text"
-					placeholder="Search by text"
-					value={text}
-					onChange={(e) => setText(e.target.value)}
-					className="p-2 border border-gray-300 rounded dark:bg-gray-800 dark:border"
-				/>
-				<input
-					type="text"
-					placeholder="Search by author"
-					value={author}
-					onChange={(e) => setAuthor(e.target.value)}
-					className="p-2 border border-gray-300 rounded dark:bg-gray-800 dark:border"
-				/>
-				<input
-					type="text"
-					placeholder="Search by category"
-					value={category}
-					onChange={(e) => setCategory(e.target.value)}
-					className="p-2 border border-gray-300 rounded dark:bg-gray-800 dark:border"
-				/>
+			<div className="text-xl grid grid-cols-1  md:grid-cols-3 gap-4 mb-6">
+				<div className="w-full">
+					<input
+						type="text"
+						placeholder="Search by text"
+						value={text}
+						onChange={(e) =>
+							handleInputChange("text", e.target.value)
+						}
+						className="p-2 w-full border border-gray-300 rounded dark:bg-gray-800 dark:border-gray-600 dark:text-white"
+					/>
+					{errors.text && (
+						<p className="text-red-500 text-base">{errors.text}</p>
+					)}
+				</div>
+				<div className="w-full">
+					<input
+						type="text"
+						placeholder="Search by author"
+						value={author}
+						onChange={(e) =>
+							handleInputChange("author", e.target.value)
+						}
+						className="p-2 w-full border border-gray-300 rounded dark:bg-gray-800 dark:border-gray-600 dark:text-white"
+					/>
+					{errors.author && (
+						<p className="text-red-500 text-base">
+							{errors.author}
+						</p>
+					)}
+				</div>
+				<div className="w-full">
+					<input
+						type="text"
+						placeholder="Search by category"
+						value={category}
+						onChange={(e) =>
+							handleInputChange("category", e.target.value)
+						}
+						className="p-2 w-full border border-gray-300 rounded dark:bg-gray-800 dark:border-gray-600 dark:text-white"
+					/>
+					{errors.category && (
+						<p className="text-red-500 text-base">
+							{errors.category}
+						</p>
+					)}
+				</div>
 			</div>
-			<div className="text-center mb-6">
+			<div className="text-center mb-6 flex justify-center gap-4">
 				<Button onClick={handleSearch} text="Search" />
+				<Button onClick={clearInputs} text="Clear inputs" />
 			</div>
 
 			{quotes.length > 0 ? (
